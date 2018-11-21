fe0465a321c4cda13731c685d3868153b4d31f9d
Sergey Samofalov <samof.sergey@gmail.com>
2018-11-21 22:21:45 +0300

    fix: remove connection pool
    Revert "feat: add connection pool"
    
    This reverts commit 1169ae8

--- src/main/java/com/moneytransfer/dao/H2DAOFactory.java
+++ src/main/java/com/moneytransfer/dao/H2DAOFactory.java
@@ -4,10 +4,8 @@ import com.moneytransfer.dao.impl.AccountDAOImpl;
 import com.moneytransfer.dao.impl.UserDAOImpl;
 import com.moneytransfer.exception.CustomException;
 import com.moneytransfer.utils.Utils;
-
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.log4j.Logger;
-import org.h2.jdbcx.JdbcConnectionPool;
 import org.h2.tools.RunScript;
 
 import java.io.FileNotFoundException;
@@ -29,17 +27,13 @@ public class H2DAOFactory extends DAOFactory {
 	private static final UserDAOImpl userDAO = new UserDAOImpl();
 	private static final AccountDAOImpl accountDAO = new AccountDAOImpl();
 
-	private static final JdbcConnectionPool connectionPool;
-
 	static  {
 		// init: load driver
 		DbUtils.loadDriver(h2_driver);
-		connectionPool = JdbcConnectionPool.create(h2_connection_url, h2_user, h2_password);
-		connectionPool.setMaxConnections(10);
 	}
 
 	public static Connection getConnection() throws SQLException {
-		return connectionPool.getConnection();
+		return DriverManager.getConnection(h2_connection_url, h2_user, h2_password);
 	}
 
 	public UserDAO getUserDAO() {
