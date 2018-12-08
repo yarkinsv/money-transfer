69b8a1c1a80a1c918457a8855ef877538a3671ea
Fedor Khodkov <fkhodkov@gmail.com>
2018-12-08 13:05:52 +0300

    Один объект класса H2DAOFactory

--- src/main/java/com/moneytransfer/dao/DAOFactory.java
+++ src/main/java/com/moneytransfer/dao/DAOFactory.java
@@ -12,14 +12,22 @@ public abstract class DAOFactory {
 
 	public abstract void populateTestData() throws CustomException;
 
+    private static H2DAOFactory h2DAOFactory = null;
+    private static DAOFactory getH2DAOFactory(){
+        if (h2DAOFactory == null)
+            h2DAOFactory = new H2DAOFactory();
+        return h2DAOFactory;
+    }
+
+
 	public static DAOFactory getDAOFactory(int factoryCode) {
 
 		switch (factoryCode) {
 		case H2:
-			return new H2DAOFactory();
+			return getH2DAOFactory();
 		default:
 			// by default using H2 in memory database
-			return new H2DAOFactory();
+			return getH2DAOFactory();
 		}
 	}
 }
