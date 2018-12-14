ce29067e88e930150629318ef1319d5c9ca60c45
Petr Eremeykin <eremeykin@gmail.com>
2018-12-15 00:19:37 +0300

    Add regular heap dump in profile.sh

--- profile.sh
+++ profile.sh
@@ -1,6 +1,7 @@
 set -x
-recd="600s" # recording duration
-rdel="20s" # recording delay
+recd="600" # recording duration
+rdel="10" # recording delay
+ddel="120" # dump delay
 
 lsofline=`lsof -i:8080 | grep java --color=never`
 lsof_regex="java\s*(\S*)"
@@ -9,15 +10,14 @@ if [[ $lsofline =~ $lsof_regex ]]
 		oldjpid="${BASH_REMATCH[1]}"
 		echo "oldjpid = $oldjpid"
 		kill $oldjpid
-		echo "ok, I killed it"
-		# exit 1
-		
+		echo "ok, I killed it"	
 else
 	echo "port 8080 is free"
 fi
 
 rm ./*.jfr
 rm ./*.plg
+rm ./*.hprof
 
 
 stage=""
@@ -35,20 +35,33 @@ fi
 java -XX:+UnlockCommercialFeatures \
 	 -XX:+FlightRecorder \
 	 -XX:+StartAttachListener \
+	 -agentlib:hprof=heap=dump,format=b,file="./heap.hprof" \
 	 -jar "./target/money-transfer-1.0.0-SNAPSHOT-shaded.jar" &>/tmp/money &
-sleep 4
+sleep 5
 jpid=$!
 echo "Java pid = $jpid"
 jfrpath="rec_d-${recd}_s-${stage}.jfr"
 logpath="log_d-${recd}_s-${stage}.plg"
 echo "JFR path: " `pwd`$jfrpath
 echo "start at " `date +%H:%M:%S`
-jcmd $jpid JFR.start duration=$recd delay=$rdel filename="$jfrpath" settings="flight-recorder.jfc" &>/tmp/jcmd
+jcmd $jpid JFR.start duration="${recd}s" delay="${rdel}s" filename="$jfrpath" settings="flight-recorder.jfc" &>/tmp/jcmd
 python3 "run_tests.py" &>$logpath &
 ppid=$!
 echo "Python pid = $ppid"
-sleep $recd
-sleep $rdel
+
+sleep $(expr $rdel + 10)
+
+count=0
+timepassed=0
+while [ $timepassed -lt  $recd ]
+do 
+	count=$(expr $count + 1)
+	dumpname="dump_${count}.hprof"
+	jmap -dump:format=b,file=$dumpname $jpid
+	sleep $ddel
+	timepassed=$(expr $timepassed + $ddel)
+done
+
 sleep 10 # guard interval
 kill  $ppid
 kill  $jpid
