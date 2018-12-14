8cd7337606ac5af1bb9ad053a91c782a62b9150f
Petr Eremeykin <eremeykin@gmail.com>
2018-12-14 20:01:35 +0300

    Add fist version of profile.sh script

---
+++ profile.sh
@@ -0,0 +1,56 @@
+recd="30s" # recording duration
+rdel="20s" # recording delay
+
+lsofline=`lsof -i:8080 | grep java --color=never`
+lsof_regex="java\s*(\S*)"
+if [[ $lsofline =~ $lsof_regex ]]
+	then
+		oldjpid="${BASH_REMATCH[1]}"
+		echo "oldjpid = $oldjpid"
+		kill $oldjpid
+		echo "ok, I killed it"
+		# exit 1
+		
+else
+	echo "port 8080 is free"
+fi
+
+rm ./*.jfr
+rm ./*.plg
+
+
+stage=""
+gitline=`git log --oneline --decorate`
+git_regex="tag: ([^,]*)"
+if [[ $gitline =~ $git_regex ]]
+	then
+		stage="${BASH_REMATCH[1]}"
+		echo $stage
+else
+	echo "cant' determinate stage"
+	exit 1
+fi
+
+java -XX:+UnlockCommercialFeatures \
+	 -XX:+FlightRecorder \
+	 -XX:+StartAttachListener \
+	 -jar "./target/money-transfer-1.0.0-SNAPSHOT-shaded.jar" &>/tmp/money &
+sleep 4
+jpid=$!
+echo "Java pid = $jpid"
+# timestamp=`date +%H-%M-%S`
+# jfrpath="rec_d-${recd}_s-${stage}_$timestamp.jfr"
+# logpath="log_d-${recd}_s-${stage}_$timestamp.plg"
+jfrpath="rec_d-${recd}_s-${stage}.jfr"
+logpath="log_d-${recd}_s-${stage}.plg"
+echo "JFR path: " `pwd`$jfrpath
+
+jcmd $jpid JFR.start duration=$recd delay=$rdel filename="$jfrpath" &>/tmp/jcmd
+python3 "run_tests.py" &>$logpath &
+ppid=$!
+echo "Python pid = $ppid"
+sleep $recd
+sleep $rdel
+sleep 10 # guard interval
+kill  $ppid
+kill  $jpid
