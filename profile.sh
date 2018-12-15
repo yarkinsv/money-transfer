c5a61fc55f7644f1b9f099d1e373ef57c913f1e3
Petr Eremeykin <eremeykin@gmail.com>
2018-12-15 09:52:19 +0300

    Add dump option to profile.sh

--- profile.sh
+++ profile.sh
@@ -3,6 +3,15 @@ recd="600" # recording duration
 rdel="10" # recording delay
 ddel="120" # dump delay
 
+if [[ $* == *--clear* ]]
+	then
+		echo "clear only"
+		rm ./*.jfr
+		rm ./*.plg
+		rm ./*.hprof
+		exit 0
+fi
+
 lsofline=`lsof -i:8080 | grep java --color=never`
 lsof_regex="java\s*(\S*)"
 if [[ $lsofline =~ $lsof_regex ]]
@@ -14,11 +23,6 @@ if [[ $lsofline =~ $lsof_regex ]]
 else
 	echo "port 8080 is free"
 fi
-
-rm ./*.jfr
-rm ./*.plg
-rm ./*.hprof
-
 
 stage=""
 gitline=`git log --oneline --decorate | grep HEAD`
@@ -42,6 +46,13 @@ jpid=$!
 echo "Java pid = $jpid"
 jfrpath="rec_d-${recd}_s-${stage}.jfr"
 logpath="log_d-${recd}_s-${stage}.plg"
+
+if [[ $* == *--dump* ]]
+	then
+		jfrpath="rec_d-${recd}_s-${stage}_dump.jfr"
+		logpath="log_d-${recd}_s-${stage}_dump.plg"
+fi
+
 echo "JFR path: " `pwd`$jfrpath
 echo "start at " `date +%H:%M:%S`
 jcmd $jpid JFR.start duration="${recd}s" delay="${rdel}s" filename="$jfrpath" settings="flight-recorder.jfc" &>/tmp/jcmd
@@ -51,16 +62,21 @@ echo "Python pid = $ppid"
 
 sleep $(expr $rdel + 10)
 
-count=0
-timepassed=0
-while [ $timepassed -lt  $recd ]
-do 
-	count=$(expr $count + 1)
-	dumpname="dump_${count}.hprof"
-	jmap -dump:format=b,file=$dumpname $jpid
-	sleep $ddel
-	timepassed=$(expr $timepassed + $ddel)
-done
+if [[ $* == *--dump* ]]
+	then
+		count=0
+		timepassed=0
+		while [ $timepassed -lt  $recd ]
+		do 
+			count=$(expr $count + 1)
+			dumpname="dump_${count}.hprof"
+			jmap -dump:format=b,file=$dumpname $jpid
+			sleep $ddel
+			timepassed=$(expr $timepassed + $ddel)
+		done
+else
+	sleep $recd
+fi
 
 sleep 10 # guard interval
 kill  $ppid
